a7d73cff50bc310d7299e19823b2be115bc12756
maielprazz <ismail.prasetyo@gmail.com>
2021-06-26 09:55:00 +0700

    01:46:01

--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
+from .models import Sale
 # Create your views here.
+def home_view(request):
+  halo = 'hello from the view'
+  return render(request, 'sales/home.html',{'h': halo})
+
+
+class SaleListView(ListView):
+  model = Sale
+  template_name = 'sales/main.html' 
+
+class SaleDetailView(DetailView):
+  model= Sale
+  template_name = 'sales/detail.html'  
